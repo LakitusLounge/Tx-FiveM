3c055ea0ca257e5f248779b1686d5cace4ad4926
LakitusLounge <154388129+LakitusLounge@users.noreply.github.com>
2023-12-30 16:46:10 -0500

    Update tx-start.sh

--- tx-start.sh
+++ tx-start.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
 
-cat << "EOF"
-       _____    _   _           _   
-      |_   _|  | | | |         | |  
-        | |_  _| |_| | ___  ___| |_ 
-        | \ \/ /  _  |/ _ \/ __| __|
-        | |>  <| | | | (_) \__ \ |_ 
-        \_/_/\_\_| |_/\___/|___/\__|
-
-     Discord : https://discord.txhost.fr
-          Site : https://txhost.fr
-            
-            TxHost © 2021 - 2022                          
-EOF
 
     sleep 2 
 
