63ea3cbbc1587a03602d9cbae4201e32ee189867
LakitusLounge <154388129+LakitusLounge@users.noreply.github.com>
2023-12-30 16:55:32 -0500

    Update tx-fivem.sh

--- tx-fivem.sh
+++ tx-fivem.sh
@@ -1,79 +1,64 @@
 #!/bin/bash
-# Script Auto Install Five M
-#=====================================================================================
+# Auto Install FiveM Script
+# =====================================================================================
 # Author:   Ma3lich#7006
-#=====================================================================================
-#=====================================================================================
+# =====================================================================================
+# =====================================================================================
 # Root Force
-# By TxHost.fr
 
-cat << "EOF"
-       _____    _   _           _   
-      |_   _|  | | | |         | |  
-        | |_  _| |_| | ___  ___| |_ 
-        | \ \/ /  _  |/ _ \/ __| __|
-        | |>  <| | | | (_) \__ \ |_ 
-        \_/_/\_\_| |_/\___/|___/\__|
-
-     Discord : https://discord.txhost.fr
-          Site : https://txhost.fr
-            
-            TxHost © 2021 - 2022                          
-EOF
-#Supported systems:
+# Supported systems:
 supported="Ubuntu (20.04)"
-COLOR1='\033[0;32m'                                         #green color
-COLOR2='\033[0;31m'                                         #red color
+COLOR1='\033[0;32m'     # green color
+COLOR2='\033[0;31m'     # red color
 COLOR3='\33[0;33m'
-NC='\033[0m'                                                #no color
+NC='\033[0m'            # no color
 
- if [ "$(id -u)" != "0" ]; then
-         printf "${RED}ERREUR : Tx-FiveM  n'a pas accès root. ⛔️\\n" 1>&2
-         printf "\\n"
-         exit 1
- fi
-    printf "${COLOR1}   ©️ Copyright Tous droits réservés TXHOST ©️ \\n"
-    printf "${COLOR2}  💻 Systèmes pris en charge : $supported 💻\\n"
-    printf "${NC}\\n"    
-    sleep 5
+if [ "$(id -u)" != "0" ]; then
+    printf "${RED}ERROR: Tx-FiveM does not have root access. ⛔️\\n" 1>&2
+    printf "\\n"
+    exit 1
+fi
+printf "${COLOR1}   ©️ Copyright All rights reserved TXHOST ©️ \\n"
+printf "${COLOR2}  💻 Supported Systems: $supported 💻\\n"
+printf "${NC}\\n"
+sleep 5
+
 #############################################################################
-#Installation prérequis
-echo -e "${YELLOW} Instalaltion des prérequis pour un serveur Five M ! \\n"
+# Prerequisite Installation
+echo -e "${YELLOW} Installing prerequisites for a FiveM server! \\n"
 sleep 1
-    apt update
-    apt upgrade -y
-    apt install bash curl wget nload htop sudo neofetch -y
-    wget https://raw.githubusercontent.com/TxHost/assets/main/vps/.bashrc
-    source .bashrc
-    rm /etc/motd
-    cd /etc/
-    wget https://raw.githubusercontent.com/TxHost/assets/main/vps/motd
-
-    sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/g' /etc/ssh/sshd_config;systemctl restart ssh
- apt install lsb-release apt-transport-https ca-certificates bash xz-utils git screen -y 
+apt update
+apt upgrade -y
+apt install bash curl wget nload htop sudo neofetch -y
+wget https://raw.githubusercontent.com/TxHost/assets/main/vps/.bashrc
+source .bashrc
+rm /etc/motd
+cd /etc/
+wget https://raw.githubusercontent.com/TxHost/assets/main/vps/motd
+sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/g' /etc/ssh/sshd_config
+systemctl restart ssh
+apt install lsb-release apt-transport-https ca-certificates bash xz-utils git screen -y
 sleep 2
 
-#Installation de 5436
-
-echo -e "${YELLOW} Instalaltion de la dernière version de l'artefact pour serveur Five M  \\n"
+# Installation of 5436
+echo -e "${YELLOW} Installing the latest artifact for FiveM server \\n"
 sleep 1
-echo -e  "${CYAN} Démarrage de l'instalaltion de version de 5436 pour serveur Five M !"
-  cd /home/
-  mkdir -p fivem
-  cd /home/fivem
-  wget  https://runtime.fivem.net/artifacts/fivem/build_proot_linux/master/5436-1984c3e2a7b968f2772a90920b56473b9995f5e3/fx.tar.xz
-  tar xvfJ fx.tar.xz
-  sed -i '1irm -r cache' run.sh
-  rm fx.tar.xz
+echo -e "${CYAN} Starting the installation of version 5436 for FiveM server!"
+cd /home/
+mkdir -p fivem
+cd /home/fivem
+wget https://runtime.fivem.net/artifacts/fivem/build_proot_linux/master/5436-1984c3e2a7b968f2772a90920b56473b9995f5e3/fx.tar.xz
+tar xvfJ fx.tar.xz
+sed -i '1irm -r cache' run.sh
+rm fx.tar.xz
 sleep 2
 
-#Installation de SYSTEMCTL
+# Installation of SYSTEMCTL
 echo
-    printf "${YELLOW} Vous souhaitez disposer de commandes system pour démarrer votre serveur fivem ❓  [o/N]\\n"
-    read reponse
-if [[ "$reponse" == "o" ]]
-then 
-printf "${CYAN} Démarrage technologie pour démarrer votre serveur fivem !"
+printf "${YELLOW} Do you want to have system commands to start your FiveM server? ❓  [Y/n]\\n"
+read reponse
+if [[ "$reponse" == "Y" || "$reponse" == "y" ]]; then
+    printf "${CYAN} Starting technology to start your FiveM server!"
     cd /etc/systemd/system
     wget https://raw.githubusercontent.com/TxHost/Tx-Fivem/master/fivem.service
     systemctl enable fivem.service
@@ -85,13 +70,12 @@ printf "${CYAN} Démarrage technologie pour démarrer votre serveur fivem !"
 fi
 sleep 2
 
-# Installation MARIADB
+# Installation of MARIADB
 echo
-    printf "${YELLOW} Souhaitez-vous créer une installation automatique de MariaDB ❓ [o/N]\\n"
-    read reponse
-if [[ "$reponse" == "o" ]]
-then 
-printf "${CYAN} Démarrage de l'instalaltion de MariaDB pour serveur FiveM !"
+printf "${YELLOW} Do you want to create an automatic installation of MariaDB? ❓ [Y/n]\\n"
+read reponse
+if [[ "$reponse" == "Y" || "$reponse" == "y" ]]; then
+    printf "${CYAN} Starting the installation of MariaDB for FiveM server!"
     apt -y install software-properties-common curl apt-transport-https ca-certificates gnupg
     LC_ALL=C.UTF-8 add-apt-repository -y ppa:ondrej/php
     add-apt-repository -y ppa:chris-lea/redis-server
@@ -101,99 +85,80 @@ printf "${CYAN} Démarrage de l'instalaltion de MariaDB pour serveur FiveM !"
     sudo apt-get update -y
     sudo apt-get install php-mbstring php-gettext
     sudo apt -y install php7.4
-    apt install -y php7.4-{cli,gd,mysql,pdo,mbstring,tokenizer,bcmath,xml,fpm,curl,zip} mariadb-client mariadb-server apache2 tar unzip git 
+    apt install -y php7.4-{cli,gd,mysql,pdo,mbstring,tokenizer,bcmath,xml,fpm,curl,zip} mariadb-client mariadb-server apache2 tar unzip git
     php -v
 fi
 sleep 2
 
-
-echo -n -e "${GREEN} Quel est le nom de votre base de données ❓ ${YELLOW}(tx_base)${reset}: "
+echo -n -e "${GREEN} What is the name of your database? ❓ ${YELLOW}(tx_base)${reset}: "
 read -r DBNAME
 if [[ "$DBNAME" == "" ]]; then
-  DBNAME="tx_base"  
+    DBNAME="tx_base"
 fi
 sleep 2
 
-
-echo -n -e "${GREEN} Quel est l'utilisateur de votre base de données ❓ ${YELLOW}(txhost)${reset}: "
+echo -n -e "${GREEN} What is the database user? ❓ ${YELLOW}(txhost)${reset}: "
 read -r DBUSER
 if [[ "$DBUSER" == "" ]]; then
-  DBUSER="txhost"  
+    DBUSER="txhost"
 fi
 sleep 2
-echo -n -e "${GREEN} Quel est le mot de passe de votre base de données ❓ ${YELLOW}(txhost) ${reset}: "
+
+echo -n -e "${GREEN} What is the database user's password? ❓ ${YELLOW}(txhost) ${reset}: "
 read -s -r DBPASS
 if [[ "$DBPASS" == "" ]]; then
-  DBUSER="txhost"  
+    DBUSER="txhost"
 fi
 
-#Installation PHPMYADMIN
+# Installation of PHPMYADMIN
 echo
-    printf "${YELLOW} Souhaitez-vous crée une installation automatique de PHPMYADMIN ❓ [o/N]\\n"
-    read reponse
-if [[ "$reponse" == "o" ]]  
-then 
-printf "${CYAN} Démarrage de l'instalaltion de PHPMYADMIN !"
+printf "${YELLOW} Do you want to create an automatic installation of PHPMYADMIN? ❓ [Y/n]\\n"
+read reponse
+if [[ "$reponse" == "Y" || "$reponse" == "y" ]]; then
+    printf "${CYAN} Starting the installation of PHPMYADMIN!"
     apt install phpmyadmin -y
     sudo service apache2 restart
     ln -s /usr/share/phpmyadmin/ /var/www/html/phpmyadmin
 fi
 
-echo -e "${YELLOW} Configuration de la utilisateur"
-  echo "Mettre le mot de passe root de MySQL"
-  sleep 2
-  mysql -e "CREATE USER '${DBUSER}'@'localhost' IDENTIFIED BY '${DBPASS}';"
-  mysql -e "CREATE DATABASE ${DBNAME};"
-  mysql -p -e "GRANT ALL PRIVILEGES ON * . * TO '${DBUSER}'@'localhost';"
-  mysql -e "FLUSH PRIVILEGES;"
+echo -e "${YELLOW} Configuring the user"
+echo "Enter the MySQL root password"
+sleep 2
+mysql -e "CREATE USER '${DBUSER}'@'localhost' IDENTIFIED BY '${DBPASS}';"
+mysql -e "CREATE DATABASE ${DBNAME};"
+mysql -p -e "GRANT ALL PRIVILEGES ON * . * TO '${DBUSER}'@'localhost';"
+mysql -e "FLUSH PRIVILEGES;"
 
+sleep 3
 
-  sleep 3
+printf "${COLOR1}✔️ Installation is complete! \\n"
+printf "${COLOR3}❤️ TxHost Website: https://txhost.fr/ \\n"
+printf "${COLOR3}❤️ TxHost Discord: https://discord.txhost.fr/ \\n"
+printf "${COLOR3}❤️ TxHost Github: https://github.com/TxHost/ \\n"
+printf "${NC}\\n"
+sleep 1
+printf "${COLOR1}✔️ MySQL Summary \\n"
+printf "${COLOR4}🌐 phpMyAdmin link: http://$(hostname -I)/phpmyadmin/ \\n"
+printf "${COLOR4}👤 MySQL Database Username: ${DBUSER}\\n"
+printf "${COLOR4}👤 MySQL Database Connection Password: ${DBPASS} \\n"
+printf "${NC}\\n"
+sleep 1
+printf "${COLOR2}✔️ Summary on creating your server! \\n"
+printf "${COLOR2}🌐 TxAdmin link: http://$(hostname -I):40120/ \\n"
+printf "${COLOR2}💻 Folder Path: /home/fivem \\n"
+printf "${COLOR2}⚠️ Do not delete run.sh and alpine\\n"
+printf "${NC}\\n"
 
-    printf "${COLOR1}✔️ L'installation est terminée ! \\n"
-    printf "${COLOR3}❤️ Site de TxHost: https://txhost.fr/ \\n"
-    printf "${COLOR3}❤️ Discord de TxHost: https://discord.txhost.fr/ \\n"
-    printf "${COLOR3}❤️ Github de TxHost: https://github.com/TxHost/ \\n"
-    printf "${NC}\\n" 
-    sleep 1
-    printf "${COLOR1}✔️ Récapitulatif du MySQL \\n"
-    printf "${COLOR4}🌐 Lien du phpMyAdmin : http://$(hostname -I)/phpmyadmin/ \\n"
-    printf "${COLOR4}👤 Nom d'utilisateur de la base de données MySQL: ${DBUSER}\\n"
-    printf "${COLOR4}👤 Mot de passe de connexion base de données MySQL: ${DBPASS} \\n"
-    printf "${NC}\\n" 
-    sleep 1
-    printf "${COLOR2}✔️ Récapitulatif sur créaction de votre seveur ! \\n"
-    printf "${COLOR2}🌐 Lien du TxAdmin : http://$(hostname -I):40120/ \\n"
-    printf "${COLOR2}💻 Chemin du dossier  : /home/fivem \\n"
-    printf "${COLOR2}⚠️ Ne surtout pas supprime run.sh et alpine\\n"
-    printf "${NC}\\n"    
-
-  sleep 10
+sleep 10
 echo
-    printf "${YELLOW} Souhaitez-vous démarrer votre serveur Five M   ❓ [o/N]\\n"
-    read reponse
-if [[ "$reponse" == "o" ]]
-then 
-printf "${CYAN} Démarrage de votre serveur Five M !"
-    sudo iptables -A INPUT -p tcp --dport 40120 -j ACCEPT  
+printf "${YELLOW} Do you want to start your FiveM server? ❓ [Y/n]\\n"
+read reponse
+if [[ "$reponse" == "Y" || "$reponse" == "y" ]]; then
+    printf "${CYAN} Starting your FiveM server!"
+    sudo iptables -A INPUT -p tcp --dport 40120 -j ACCEPT
     sudo netfilter-persistent save
     sudo netfilter-persistent reload
     cd /home/fivem
     wget https://raw.githubusercontent.com/TxHost/assets/main/fivem/server.cfg
     bash /home/fivem/run.sh
 fi
-
-
-cat << "EOF"
-       _____    _   _           _   
-      |_   _|  | | | |         | |  
-        | |_  _| |_| | ___  ___| |_ 
-        | \ \/ /  _  |/ _ \/ __| __|
-        | |>  <| | | | (_) \__ \ |_ 
-        \_/_/\_\_| |_/\___/|___/\__|
-
-     Discord : https://discord.txhost.fr
-          Site : https://txhost.fr
-            
-            TxHost © 2021 - 2022                          
-EOF
